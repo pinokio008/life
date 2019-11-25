bc0f67c727e98da3012708e6ec6f82751a4a9876
pinokio008 <bossatoy.0807@gmail.com>
2019-11-25 17:11:40 +0900

    third commit

---
+++ app/models/post.rb
@@ -0,0 +1,19 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+class Post < ApplicationRecord
+  def self.useRFQ(uri, host, key)
+    url = URI(uri)
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = host
+    request["x-rapidapi-key"] = key
+
+    return http.request(request)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index', to: 'posts#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'home#top'
+
   get 'about', to: 'home#about'
+  get 'posts/api', to: 'posts#api'
+  get 'posts/hesitation', to: 'posts#hesitation'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
