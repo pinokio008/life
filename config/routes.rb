beb4792b5bc5498060c266886d46b295528976fc
pinokio008 <bossatoy.0807@gmail.com>
2019-12-13 19:21:43 +0900

    add_delete

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   get 'posts/hesitation', to: 'posts#hesitation'
   get 'posts/:id/api', to: 'posts#api', as: 'api'
   get 'posts/:id', to: 'posts#show', as: 'post'
-  post 'posts', to: 'posts#create'
   patch 'posts/:id', to: 'posts#update'
   put 'posts/:id', to: 'posts#update'
+  delete 'posts/:id', to: 'posts#delete'
+  post 'posts', to: 'posts#create'
   get 'users/:id', to: 'users#show', as: 'user'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
